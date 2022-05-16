870194dbdc1d90b5236b2b96ead35738ff567cf6
akshay2807 <akshay.l@appinessworld.com>
2022-05-16 10:26:47 +0530

    Section 22

---
+++ Javascript/Arrow functions/arrow_fn.js
@@ -0,0 +1,30 @@
+//ARROW function
+// const add = (x, y) => {
+//     return x + y;
+// }
+
+// const square = x => {
+//     return x * x;
+// }
+
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) + 1
+// }
+
+//setTimeout & setInterval functions
+// setTimeout(() => {
+//     console.log("Hello!!")
+// }, 3000)
+
+// console.log("Hello!!")
+// setTimeout(() => {
+//     console.log(".....are you still there")
+// }, 3000)
+// console.log("Goodbye!!")
+
+//SET_INTERVAL
+// const id = setInterval(() => {
+//     console.log(Math.random())
+// }, 2000)
+
+// clearInterval(id)
