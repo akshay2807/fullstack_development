da64f24a1391f9dcc0f9998d7a6eb52681f80402
akshay2807 <akshay.l@appinessworld.com>
2022-05-16 10:37:51 +0530

    section 25

---
+++ Javascript/change_event/app.js
@@ -0,0 +1,12 @@
+const input = document.querySelector('input');
+const h1 = document.querySelector('h1');
+
+// input.addEventListener('change', function (e) {
+//     console.log("ahgschnn")
+// });
+
+input.addEventListener('input', function (e) {
+    h1.innerText = input.value;
+    //console.log("INPUT EVENT!");
+    //console.log(e)
+});
