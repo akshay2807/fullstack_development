bcfc8e24e2e5f837389015626328868f80bc7d3e
akshay2807 <akshay.l@appinessworld.com>
2022-05-16 12:01:59 +0530

    section 28

---
+++ Javascript/XHRs/app.js
@@ -0,0 +1,17 @@
+// A simple request to one of the swapi end points
+
+const req = new XMLHttpRequest();
+
+req.onload = function () {
+    console.log("It Loaded!!!!");
+    const data = JSON.parse(this.responseText);
+    console.log(data.name, data.height);
+};
+
+req.onerror = function () {
+    console.log("ERROR!!!!");
+    console.log(this);
+};
+
+req.open("GET", "https://swapi.dev/api/people/1/");
+req.send();
