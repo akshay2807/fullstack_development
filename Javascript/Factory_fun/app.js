0c98680cce52d9805ec9ae2033b7066f51d088e2
akshay2807 <akshay.l@appinessworld.com>
2022-05-17 09:23:57 +0530

    section 29

--- Javascript/Factory_fun/app.js
+++ Javascript/Factory_fun/app.js
@@ -65,6 +65,7 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        this.calcHSL();
         // console.log('Inside Constructor')
         // console.log(r, g, b);
     }
@@ -87,9 +88,66 @@ class Color {
             '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
         );
     }
+    hsl() {
+        const { h, s, l } = this;
+        return `hsl(${h},${s}%,${l}%)`;
+    }
+    fullySaturated() {
+        const { h, l } = this;
+        return `hsl(${h},100%,${l}%)`;
+    }
+    opposite() {
+        const { h, s, l } = this;
+        const newHue = (h + 180) % 360;
+        return `hsl(${newHue},${s}%,${l}%)`;
+    }
+    calcHSL() {
+        let { r, g, b } = this;
+        //make r, g, & b fractions of 1
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        //finding greatest & smallest channel values
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            //red is max
+            h = ((g - b) / delta % 6);
+        //green is max
+        else if (cmax == g)
+            //green is max
+            h = (b - r) / delta + 2;
+        else
+            //Blue is max
+            h = (r - g) / delta + 4;
+
+        h = Math.round(h * 60);
+        //Make negative hues positive behind 360
+        if (h < 0) h += 360;
+        //calculate lightness
+        l = (cmax + cmin) / 2;
+
+        //Calculate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        //multiply l and s by 100
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l;
+    }
     // greet() {
     //     return `Hello form ${this.name}!`;
     // }//were grouping everthing inside a class 
 }
-const red = new Color(255, 67, 89, 'tomato')
-const white = new Color(255, 67, 89, 'white')
+const red = new Color(255, 67, 89, 'tomato');
+const white = new Color(255, 67, 89, 'white');
+
+
